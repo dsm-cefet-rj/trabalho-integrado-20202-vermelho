4083689a33e8ebe407f8e3a2da0e6f9dfe162e43
Dennis Rodrigues <dennissantosr@gmail.com>
2021-03-08 16:42:55 -0300

    Adicinados estados a outros componentes

--- src/components/CriaRefeicao.jsx
+++ src/components/CriaRefeicao.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'; // Hook que irei usar para passar o path
 
 import '../styles/CriaRefeicao.css'
 
 export default props => {
     const history = useHistory(); // Para cria a rota no botão
+
+    const [nome, setNome] = useState("");
+    const [tipo, setTipo] = useState("")
 
     return(
         <div id="cria-refeicao">
@@ -16,18 +19,20 @@ export default props => {
                 <div className="form-group">
                     <label for="formGroupExampleInput" className="label-title">Nome: </label>
                     <br/>
-                    <input type="text" className="form-control" id="formGroupExampleInput" placeholder=""/>
+                    <input type="text" className="form-control" id="formGroupExampleInput" placeholder="" 
+                    onChange={(e) => setNome(e.target.value)} />
                 </div>
                 <br/>
                 <div className="form-group">
                     <label for="formGroupExampleInput2" className="label-title">Tipo: </label>
                     <br/>
-                    <input type="text" className="form-control" id="formGroupExampleInput2" placeholder=""/>
+                    <input type="text" className="form-control" id="formGroupExampleInput2" placeholder=""
+                    onChange={(e) => setTipo(e.target.value)} />
                 </div>
                 <br/>
                 <div className="botoes">
                     <button type="button" onClick={() => history.push('/cardapio')} className="btn btn-primary"><div className="botao">Cria</div></button>
-                    <button type="button" onClick="" className="btn btn-danger"><div className="botao">Cancela</div></button>
+                    <button type="button" onClick={() => history.push('/cardapio')} className="btn btn-danger"><div className="botao">Cancela</div></button>
                 </div>
             </form>
     </div>
--- src/components/RealizaPedido.jsx
+++ src/components/RealizaPedido.jsx
@@ -1,10 +1,13 @@
- import React from 'react'
+ import React, { useState } from 'react'
  import { useHistory } from 'react-router-dom'; // Hook que irei usar para passar o path
 
  import '../styles/RealizaPedido.css'
 
  export default props => {
     const history = useHistory(); // Para criar a rota no botão
+
+    const [quarto,setQuarto] = useState("")
+
 
     return (
         
@@ -31,7 +34,8 @@
     <form>
         <div className="form-group col-md-2">
             <label className="quarto" for="inputZip">Quarto: </label>
-            <input type="text" className="form-control" id="inputZip"/>
+            <input type="text" className="form-control" id="inputZip"
+            onChange={(e) => setQuarto(e.target.value)} value={quarto}/>
         </div>
     </form>
         <br/>
@@ -42,8 +46,11 @@
     <br/>
 
     <div className="botoes">
-        <button type="button" onClick={() => history.push('/cardapio')} className="btn btn-primary"><div className="botao">Pedir</div></button>
-        <button type="button" className="btn btn-danger"> <div className="botao">Cancela</div></button>
+        <button type="button" className="btn btn-primary"
+        onClick={() => history.push('/cardapio')}><div className="botao">Pedir</div></button>
+
+        <button type="button" className="btn btn-danger" 
+        onClick={() => history.push('/cardapio')}> <div className="botao">Cancela</div></button>
     </div>
 
     </div>
