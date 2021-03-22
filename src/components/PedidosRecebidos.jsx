fcceb0706e9e07cd64292eb29ad8257814eabff9
Dennis Rodrigues <dennissantosr@gmail.com>
2021-03-22 17:38:11 -0300

    Corrigindo PedidosRecebidos c/ Redux

--- src/components/PedidosRecebidos.jsx
+++ src/components/PedidosRecebidos.jsx
@@ -4,41 +4,35 @@ import Pedido from './Pedido.jsx'
 //import PedidosEx from '../../db.json/PedidosEx'
 
 import { useSelector, useDispatch } from 'react-redux'
-import { alterapedidoState } from '../store/reducers/pedidosrecebidosSlice'
+import { alteraPedidos } from '../store/reducers/pedidosrecebidosSlice'
 
 import '../styles/PedidosRecebidos.css'
 
 export default function PedidosRecebidos(props) {
-    const pedidos = useSelector(state => state.pedidos)
+    const pedidosRecebidos = useSelector(state => state.pedidosrecebidos)
+    const { pedidos } = pedidosRecebidos
+    console.log(pedidosRecebidos)
+    console.log(pedidos)
     const dispatch = useDispatch()
-    /*
-    const Atualiza = () => { // Funcao atualiza irá jogar cada objeto em pedidos e depois 'objetos'
-        const [...pedidos]= PedidosEx // spread vai pegar cada objeto
-        setObjeto(pedidos); // Será armazenado na variável local 'objetos'       
-    }*/
-    const { pedidoState } = props
 
-    props.alteraPedido({ola: 'hahaha'})
 
     const Renderiza = pedido => {
-        // Cria uma variavel local pedidoState, que tem uma chave pedidos que recebe os valores no objetos pedido
-        // A função lidaPronto muda o valor em 'pronto' de FALSE para TRUE
-        // SE pronto for FALSE as props são passadas para o componente filho
-        props.alteraPedido({
-            pedidos: pedido
+        // Resumidamente, o map irá pegar um objeto que será passado para pedidos cada vez que a funcao for chamada
+        dispatch(alteraPedidos)({ // armazena localmanete os dados da consulta no bd para passar pras props
+            pedidos: pedido // Valor sera o objeto do map
         })
-        console.log(props.pedidoState)
-        const lidaPronto = () => {
-            props.alteraPedido({
+
+        const lidaPronto = () => { // A funcao pronto muda o estado do pedido alterando apenas 'pronto'
+            dispatch(alteraPedidos({
                 pedidos: pedido
             },
-                pedidoState.pedidos.pronto = !pedidoState.pedidos.pronto,
-            )
+                pedidos.pronto = !pedidos.pronto,
+            ))
         }
-        if (!pedidoState.pedidos.pronto) {
+        if (!pedidos.pronto) {
             return (
-                <Pedido numero={pedidoState.pedidos.numero} quarto={pedidoState.pedidos.quarto} pedidos={pedidoState.pedidos.pedidos}
-                    total={pedidoState.pedidos.total} pronto={pedidoState.pedidos.pronto} lidaPronto={lidaPronto}></Pedido>
+                <Pedido numero={pedidos.numero} quarto={pedidos.quarto} pedidos={pedidos.pedidos}
+                    total={pedidos.total} pronto={pedidos.pronto} lidaPronto={lidaPronto}></Pedido>
             )
         }
     }
@@ -48,10 +42,15 @@ export default function PedidosRecebidos(props) {
             <div className="pedidos-recebidos-titulo">
                 <h3>Pedidos Recebidos <button className='refresh'>Atualizar</button></h3>
             </div>
-
+            {pedidos === undefined ? // Caso não pegue nada do backend
             <div>
-                {pedidos.map(Renderiza)}
+              Sem novos pedido!
             </div>
+            :  
+             <div>
+                {pedidos.map(Renderiza)}
+            </div>}
+          
             <br />
         </div>
 
--- src/store/reducers/pedidosrecebidosSlice.js
+++ src/store/reducers/pedidosrecebidosSlice.js
@@ -1,26 +1,28 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
-const pedidosrecebidos = {
-    pedidos : {}
-}
+const pedidosrecebidos = []
 
 // Buscando no json-server
-export const fetchPedidos = createAsyncThunk('pedidosrecebidos/fetchpedidos',
+export const fetchPedidos = createAsyncThunk('pedidosrecebidos/fetchPedidos',
     async () => {
-        return await( await fetch('http://localhost:3000/pedidos')).json() // await da promisse e do .json
+        return await( await fetch('http://localhost:3000/pedidosEx')).json() // await da promisse e do .json
     })
+
+function fullfillPedidosRecebidosReducer(pedidosState, pedidosFetched){
+    return pedidosFetched
+}
 
 
 export const pedidosrecebidosSlice = createSlice({ // retorna um objeto que contem os action creators e reducers
     name: 'pedidosrecebidos',
     initialState: pedidosrecebidos,
     reducers: { 
-        alteraPedidos: (state, action) => ({pedidos: action.payload})
+        alteraPedido: (state, action) => ({...state, pronto: action.payload})
      }, 
     extraReducers: {
-        [fetchPedidos.fullfiled]: (state,action) => {state = action.payload}
+        [fetchPedidos.fullfiled]: (state,action) => fullfillPedidosRecebidosReducer(state, action.payload) // Onde colocamos o reducer do fetch
     }
     })
 
-export const pedidos = pedidosrecebidosSlice.actions
+export const { alteraPedidos } = pedidosrecebidosSlice.actions
 export default pedidosrecebidosSlice.reducer
